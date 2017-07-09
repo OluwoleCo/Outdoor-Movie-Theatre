b8ccc62c358b6b50ed5ac6fc5bad9ec616457fe9
Oluwole Oyekanmi <wolexzie@gmail.com>
2017-07-10 00:27:11 +0100

    Bootstrap App

---
+++ Components/Scenes/MainMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-vr';
+import MainMenuContainer from './Layouts/MainMenuContainer.js';
+
+export default class MainMenu extends React.Component {
+  render() {
+    return (
+      <MainMenuContainer/>
+    )
+  }
+}
